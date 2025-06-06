6bdf185a419c5317972f425dd90794ffeeefe368
오승준 <skku03062@g.skku.edu>
2025-06-06 21:31:42 +0900

    init commit

---
+++ src/main/java/com/runningRank/runningRank/RunningRankApplication.java
@@ -0,0 +1,13 @@
+package com.runningRank.runningRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningRankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runningRank/runningRank/RunningRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runningRank.runningRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
