e3690b1e8152b87ff39fa7e044e068a4157d9c63
오승준 <skku03062@g.skku.edu>
2025-06-09 15:44:03 +0900

    User 엔티티 추가

---
+++ src/main/java/com/runningRank/user/domain/Gender.java
@@ -0,0 +1,5 @@
+package com.runningRank.user.domain;
+
+public enum Gender {
+    MALE,FEMAIL
+}
---
+++ src/main/java/com/runningRank/user/domain/User.java
@@ -0,0 +1,65 @@
+package com.runningRank.user.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    /**
+     *  1. PK
+     *  2. 계정
+     *  3. 비밀번호
+     *  + 3 - 2. 소셜 로그인용 outhProvider
+     *  + 3 - 3. 소셜 로그인용 outhId
+     *  4. 이름
+     *  5. 나이
+     *  6. 성별
+     *  7. 학교
+     *  8. 학번
+     *  9. 전공
+     *  10. 프로필 이미지
+     */
+
+    // 1
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 2
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // 3
+    private String password;
+
+    // 3 - 2
+    private String oauthProvider;
+
+    // 3 - 3
+    private String oauthId;
+
+    // 4
+    @Column(nullable = false)
+    private String name;
+
+    // 5
+    private int age;
+
+    // 6
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    // 7
+    @Enumerated(EnumType.STRING)
+    private School school;
+
+    // 8
+    private String studentNumber;
+
+    // 9
+    private String major;
+
+    // 10
+    private String profileImageUrl;
+
+}
