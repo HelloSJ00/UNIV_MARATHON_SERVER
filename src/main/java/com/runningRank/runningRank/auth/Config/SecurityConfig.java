609f4ed9a1e4fd52144014d0ca9035fd3c38f3bc
오승준 <skku03062@g.skku.edu>
2025-06-18 02:29:48 +0900

    fix :JwtAuthenticationFilter 중복 실행되던 버그 수정

--- src/main/java/com/runningRank/runningRank/auth/Config/SecurityConfig.java
+++ src/main/java/com/runningRank/runningRank/auth/Config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,7 +27,10 @@ public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final CustomUserDetailsService userDetailsService;
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http,
                                                    CorsConfigurationSource corsConfigurationSource) throws Exception {
@@ -69,4 +74,6 @@ public class SecurityConfig {
 
         return source;
     }
+
+
 }
