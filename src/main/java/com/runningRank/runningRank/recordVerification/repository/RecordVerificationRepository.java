31f3052817f3b74200019a1372b66e0281e14e9f
오승준 <skku03062@g.skku.edu>
2025-06-20 00:10:52 +0900

    fix : 유니크 제약조건에 따라서 기록 삭제-플러시-추가 순서로 변경

--- src/main/java/com/runningRank/runningRank/recordVerification/repository/RecordVerificationRepository.java
+++ src/main/java/com/runningRank/runningRank/recordVerification/repository/RecordVerificationRepository.java
@@ -2,10 +2,14 @@ package com.runningRank.runningRank.recordVerification.repository;
 
 import com.runningRank.runningRank.emailVerification.domain.VerificationStatus;
 import com.runningRank.runningRank.recordVerification.domain.RecordVerification;
+import com.runningRank.runningRank.runningRecord.domain.RunningType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecordVerificationRepository extends JpaRepository<RecordVerification,Long> {
     Page<RecordVerification> findByStatus(VerificationStatus status, Pageable pageable);
+    Optional<RecordVerification >findByUserIdAndRunningTypeAndStatus(Long userId, RunningType runningType,VerificationStatus status);
 }
